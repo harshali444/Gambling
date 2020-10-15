3b220dad3be7cc05e4955a2ff8156002fa48f6fc
harshali444 <jadhav.h0564@gmail.com>
2020-10-15 10:31:37 +0530

    GamblingSimulator.java file added

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -1,8 +1,8 @@
 public class GamblingSimulator {
-	
+
 	 public static void main(String[] args) {
 		 System.out.println("Welcome to Gambling Simulator");
-		 final int stake=100;
-		 int bets=1;
-		 }
-	 }
+		 final int STAKE=100;
+		 final int BETS=1;
+	}
+}
